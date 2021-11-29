f1e835d590f2b0f722c05e07fa5ff4be2fe80c91
ebokelbe <ethan.bokelberg@gmail.com>
2021-11-29 17:00:20 -0500

    modify renderAction to have separate +/-, add constructor and add/removeTrack

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,13 +2,28 @@ import React from "react";
 import './Track.css';
 
 export class Track extends React.Component{
+    constructor(props){
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+    }
+    
     renderAction(){
-        // if there are issues with rendering, this is likely the cause
-        let content = '';
-        this.props.isRemoval ? content = '-' : content = '+';
-        return (
-            <button>{content}</button>
-        )
+        if (this.props.isRemoval) {
+            return (<button className="Track-action" onClick={this.removeTrack}> - </button>);
+          }
+          else {
+            return (<button className="Track-action" onClick={this.addTrack}> + </button>);
+          }
+        }
+
+    addTrack(){
+        // add this.props.track to playlist
+        this.props.onAdd(this.props.track)
+    }
+
+    removeTrack(){
+        this.props.onRemove(this.props.track)
     }
     
     render(){
@@ -18,7 +33,7 @@ export class Track extends React.Component{
                     <h3><!-- track name will go here --></h3>
                     <p><!-- track artist will go here--> | <!-- track album will go here --></p>
                 </div>
-                <button className="Track-action"><!-- + or - will go here --></button>
+                {this.renderAction()}
             </div>
         )
     }
