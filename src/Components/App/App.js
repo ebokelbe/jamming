9cc09166d650ecea7e216c6f0d096d2e35ff625f
ebokelbe <ethan.bokelberg@gmail.com>
2021-11-29 16:15:07 -0500

    added constructor, imports and pass search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar'
-//import searchresults
-//import playlist
+import SearchResults from '../SearchResults/SearchResults'
+import Playlist from '../Playlist/Playlist'
 
 export class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {searchResults : [{name: 'Song 1', artist: 'artist 1', album: 'album 1', id: 1}, 
+    {name: 'Song 2', artist: 'artist 2', album: 'album 2', id: 2}, 
+    {name: 'Song 3', artist: 'artist 3', album: 'album 3', id: 3}]}
+  }
+  
   render(){
     return (
       <div>
@@ -12,7 +19,7 @@ export class App extends React.Component{
   <div className="App">
     <SearchBar />
     <div className="App-playlist">
-      <SearchResults />
+      <SearchResults searchResults={this.state.searchResults}/>
       <Playlist />
     </div>
   </div>
