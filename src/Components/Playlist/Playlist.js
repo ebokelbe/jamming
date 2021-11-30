6a459b00b3a736a3389defa3365050f9bf498189
ebokelbe <ethan.bokelberg@gmail.com>
2021-11-30 14:20:43 -0500

    added constructor and handleNameChange

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,10 +3,20 @@ import './Playlist.css';
 import TrackList from "../TrackList/TrackList";
 
 export class Playlist extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    
+    handleNameChange(e){
+        //accept event that is triggered by an onChange attribute in input
+        this.props.OnNameChange(e.target.value);
+    }
+    
     render(){
         return (
             <div className="Playlist">
-                <input value={this.props.defaultValue}/>
+                <input value={this.props.defaultValue} onChange={this.handleNameChange}/>
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
